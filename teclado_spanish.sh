15a83167c6d2fc69c9ed32e822d196e9929102eb
ComandPromt <vayase_a_ferrol@hotmail.com>
2019-01-26 22:09:23 +0100

    Update teclado_spanish.sh

--- teclado_spanish.sh
+++ teclado_spanish.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-### BEGIN INIT INFO
-# Provides: firewall
-# Required-Start: $syslog $remote_fs $network
-# Required-Stop: $syslog $remote_fs $network
-# Default-Start: 2 3 4 5
-# Default-Stop: 0 1 6
-# Description: Start firewall configuration
-### END INIT INFO
 setxkbmap -layout 'es,es' -model pc105
 exit 0
